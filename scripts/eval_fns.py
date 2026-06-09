337ccf2caebaa2d5658edfc5d74dff61a9699e2d
Anantha Rao <anantha.nalin.rao@gmail.com>
2026-06-09 01:54:00 -0700

    added more helpful requirements file and made some changes to the eval_fns.py script

--- scripts/eval_fns.py
+++ scripts/eval_fns.py
@@ -1,35 +1,103 @@
-from nltk.translate.bleu_score import sentence_bleu
+import evaluate
+#from nltk.translate.bleu_score import sentence_bleu
+#from nltk.translate.chrf_score import sentence_chrf
 
-def bleu(hypothesis_sent:str, human_reference:str):
-    """Computes the NLTK bleu score for BLEU-2, BLEU-3, and BLEU-4
+# Things to load beforehand:
+_chrf = evaluate.load("chrf")
+_bleu = evaluate.load("bleu")
+
+
+# def nltk_bleu(MT_sent:str, human_reference:str):
+#     """Computes the NLTK bleu score for BLEU-2, BLEU-3, and BLEU-4
+
+#     Args:
+#         - MT_sent: the machine translated sentence
+#         - human_reference: the gold reference / human translation
+    
+#     Returns:
+#     A dictionary with BLEU-2, BLEU-3, and BLEU-4 score
+#     """
+
+#     hypo = MT_sent.split(" ")
+#     ref = human_reference.split(" ")
+
+#     b2, b3, b4 = sentence_bleu(
+#     [ref],
+#     hypo,
+#     weights=[
+#         (1/2,1/2),
+#         (1/3,1/3,1/3),
+#         (1/4,1/4,1/4,1/4)
+#         ]
+#     )
+
+#     return {
+#         "bleu2": b2, 
+#         "bleu3": b3,
+#         "bleu4": b4
+#     }
+
+def bleu(MT_sent:str, human_reference:str):
+    """Computes the bleu score for BLEU-1, BLEU-2, BLEU-3, and BLEU-4
+
     Args:
-        - hypothesis_sent: the machine translated sentence
+        - MT_sent: the machine translated sentence
         - human_reference: the gold reference / human translation
     
     Returns:
-    A dictionary with BLEU-2, BLEU-3, and BLEU-4
+    A dictionary with BLEU scores and precision
     """
-
-    hypo = hypothesis_sent.split(" ")
-    ref = human_reference.split(" ")
-
-    b2, b3, b4 = sentence_bleu(
-    [ref],
-    hypo,
-    weights=[
-        (1/2,1/2),
-        (1/3,1/3,1/3),
-        (1/4,1/4,1/4,1/4)
-        ]
+    bleu_scores = _bleu.compute(
+        predictions = [MT_sent],
+        references = [human_reference],
+        smooth = True
     )
 
     return {
-        "bleu2": b2, 
-        "bleu3": b3,
-        "bleu4": b4
+        "bleu": bleu_scores["bleu"],
+        "bleu1_precision": bleu_scores["precisions"][0],
+        "bleu2_precision": bleu_scores["bleu"][1], 
+        "bleu3_precision": bleu_scores["bleu"][2],
+        "bleu4_precision": bleu_scores["bleu"][3]
     }
 
+def chrf(MT_sent:str, human_reference:str):
+    """Computes chrf++. The ++ represents the word order the character level
+    precision, recall, and f are computed over.
+    
+    Args:
+        - MT_sent: the machine translated sentence
+        - human_reference: the gold reference / human translation
+    
+    Returns:
+    A dictionary with chrf, and chrf++ scores
+    """
 
+    chrf_scores = _chrf.compute(
+        predictions = [MT_sent],
+        references = [human_reference],
+        word_order = 0
+    )
 
+    chrfplusplus_scores = _chrf.compute(
+        predictions = [MT_sent],
+        references = [human_reference],
+        word_order = 2
+    )
 
+    return {
+        "chrf": chrf_scores["score"],
+        "chrf++": chrfplusplus_scores["score"]
+    }
+
+def comet(MT_sent:str, human_reference:str):
+    """Computes Comet for a pair of sentences.
+    
+    Args:
+        - MT_sent: the machine translated sentence
+        - human_reference: the gold reference / human translation
+    
+    Returns:
+    A dictionary with COMET scores
+    """
 
